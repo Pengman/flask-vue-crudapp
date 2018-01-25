260b479b712d2b7a9556a82188903b33a71553f5
Søren Dam Pedersen <Soeren.pedersen@aalborg.dk>
2018-01-25 15:47:40 +0100

    Cleanup, Axios and new references

--- app/static/app/app.js
+++ app/static/app/app.js
@@ -1,95 +1,24 @@
-// var NewPersonRow = Vue.extend({
-//   components: {
-//     datepicker : VueStrap.datepicker
-//   },
-//   props: {
-//     person: Object,
-//     state: {
-//       type: String,
-//       default: 'default'
-//     }
-//   },
-//   created:function(){
-//     this.initData();
-//   },
-//   methods: {
-//     initData: function(){
-//       this.person = {firstName: '', lastName:'', dateOfBirth:'', zipCode:''};
-//       this.errorMessages = {firstName: '', lastName:'', dateOfBirth:'', zipCode:''};
-//     },
-//     // Manage component state. Used for default, edit, and deleting states
-//     updateState: function(state){
-//       this.state = state;
-//     },
-//     // send api request to actually update state
-//     addPerson:function(){
-//         this.updateState('loading');
-//         // GET /someUrl
-//         this.$http.post('/api/person', this.person).then((response) => {
-//             // success callback
-//             var newPerson = JSON.parse(response.body);
-
-//             // add new person to start of list
-//             this.$dispatch('remove-person', newPerson)
-
-//             this.initData();
-//             // Turn of loading
-//             this.updateState('default');
-//         }, (response) => {
-//           console.log(response)
-//             // error callback
-//             this.newPerson.loading = false;
-//         });
-//     }
-//   },
-//   template: ` <tr>
-//                 <form v-on:submit.prevent v-on:submit="addPerson()">
-//                   <td >
-
-//                   </td>
-//                   <td >
-//                     <input type="text" required class="form-control" v-model="person.firstName" placeholder="First Name">
-//                     <div class="error-message" v-if="state == 'editing'"> {{ errorMessages.firstName }} </div>
-//                   </td>
-//                   <td >
-//                     <input type="text" required class="form-control" v-model="person.lastName" placeholder="First Name">
-//                     <div class="error-message" v-if="state == 'editing'"> {{ errorMessages.lastName }} </div>
-//                   </td>
-//                   <td >
-//                     <datepicker :value.sync="person.dateOfBirth" ></datepicker>
-//                     <div class="error-message" v-if="state == 'editing'"> {{ errorMessages.dateOfBirth }} </div>
-//                   </td>
-//                   <td >
-//                     <input type="text" required class="form-control" v-model="person.zipCode" placeholder="Zp Code">
-//                     <div class="error-message" v-if="state == 'editing'"> {{ errorMessages.zipCode }} </div>
-//                   </td>
-
-//                   <td v-if="state == 'default'">
-//                     <button type="submit" class="btn btn-xs btn-success" v-bind:class="{ 'disabled': state == 'loading' }"  >
-//                       Add <i v-if="state == 'loading'" class="glyphicon glyphicon-refresh spin"></i>
-//                     </button>
-//                   </td>
-//                 </form>
-//               </tr>`
-// })
-
 var PersonRow = Vue.extend({
   components: {
     datepicker : VueStrap.datepicker
   },
   props: {
-    person: Object,
-    state: {
-      type: String,
-      default: 'default'
-    }
+    person: Object//,
+    // state: {
+    //   type: String,
+    //   default: 'default'
+    // }
   },
+    data:function(){
+	return {state: 'default', editPerson: {}};
+    },
   created:function(){
     this.initData();
   },
   methods: {
     initData: function(){
-      this.editPerson = JSON.parse(JSON.stringify(this.person));
+	this.editPerson = JSON.parse(JSON.stringify(this.person));
+	//this.state = "default";
       this.errorMessages = {firstName: '', lastName:'', dateOfBirth:'', zipCode:''};
     },
     // Manage component state. Used for default, edit, and deleting states
@@ -98,20 +27,20 @@ var PersonRow = Vue.extend({
     },
     // send api request to actually update state
     updatePerson:function(){
-        this.$http.put('/api/person/' + this.person.id, this.editPerson).then((response) => {
+        axios.put('/api/person/' + this.person.id, this.editPerson).then((response) => {
             // success callback
 
             // set component state back to default
             this.updateState('default');
 
             // update the person objects
-            this.person = JSON.parse(response.body);
-
+//            this.person = response.body;
+	    this.$emit('updated', {id: this.person.id, newPerson: response.data})
           // Re init data
-            this.initData();
+  //          this.initData();
 
         }, (response) => {
-            var response = JSON.parse(response.body);
+            var response = response.body;
             this.errorMessages = Object.assign({}, this.errorMessages, response.message)
             console.log(this.errorMessages)
 
@@ -120,9 +49,9 @@ var PersonRow = Vue.extend({
     // send api request to actually delete user
     deletePerson:function(){
         // GET /someUrl
-        this.$http.delete('/api/person/'+this.person.id).then((response) => {
+        axios.delete('/api/person/'+this.person.id).then((response) => {
             // success callback
-            this.$dispatch('remove-person', {id: this.person.id})
+            this.$emit('removed', {id: this.person.id})
         }, (response) => {
             // error callback
         });
@@ -144,7 +73,7 @@ var PersonRow = Vue.extend({
                 </td>
                 <td >
                   <span  v-if="state != 'editing'"> {{ person.dateOfBirth }} </span>
-                  <datepicker v-if="state == 'editing'" :value.sync="editPerson.dateOfBirth" ></datepicker>
+                  <datepicker v-if="state == 'editing'" v-model="editPerson.dateOfBirth" ></datepicker>
                   <div class="error-message" v-if="state == 'editing'"> {{ errorMessages.dateOfBirth }} </div>
                 </td>
                 <td >
@@ -186,7 +115,8 @@ var vm = new Vue({
   },
 
   // Anything within the ready function will run when the application loads
-  ready: function() {
+  // mounted hook in 2.0
+  mounted: function() {
     this.getPeople();
   },
 
@@ -194,20 +124,28 @@ var vm = new Vue({
   methods: {
     getPeople:function(){
         // GET /someUrl
-        this.$http.get('/api/person').then((response) => {
+        axios.get('/api/person').then((response) => {
             // success callback
-            this.people = JSON.parse( response.data );
+            this.people = response.data ;
         }, (response) => {
             // error callback
         });
     },
-    removePerson:function(id){
-      var personIndex = this.findPersonIndexById(id);
+    removePerson:function(args){
+      var personIndex = this.findPersonIndexById(args.id);
       if(personIndex > -1 ){
         this.people.splice(personIndex ,1)
       }
     },
-    findPersonIndexById:function(id){
+    updatePerson:function(args){
+      var personIndex = this.findPersonIndexById(args.id);
+      if(personIndex > -1 ){
+          //this.people[personIndex] = args.newPerson;
+	  this.people.splice(personIndex,1,args.newPerson);
+      }
+    },
+
+      findPersonIndexById:function(id){
       // Check each person for id, if found return that index. Otherwise return -1
       for (var i=0; i < this.people.length; i++) {
           if (this.people[i].id === id) {
@@ -221,9 +159,9 @@ var vm = new Vue({
         this.newPerson.loading = true;
         var person = this.newPerson;
         // GET /someUrl
-        this.$http.post('/api/person', person).then((response) => {
+        axios.post('/api/person', person).then((response) => {
             // success callback
-            var newPerson = JSON.parse(response.body);
+            var newPerson = response.data;
 
             // Turn of loading
             this.newPerson.loading = false;
@@ -244,7 +182,7 @@ var vm = new Vue({
 
     }
   },
-
+/*
   events: {
     'remove-person': function (data) {
       if(data && data.id){
@@ -256,5 +194,5 @@ var vm = new Vue({
         this.people.unshift(newPerson);
       }
     }
-  }
+  }*/
 });
